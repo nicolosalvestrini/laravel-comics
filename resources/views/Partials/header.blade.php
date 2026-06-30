3d634ae5db298659a4ae129aa0e83d1f1a0b03a6
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-07-01 00:19:46 +0200

    Add header layout and logo image to the navigation

--- resources/views/Partials/header.blade.php
+++ resources/views/Partials/header.blade.php
@@ -2,17 +2,29 @@
     $navbarItems = config('navbar');
 @endphp
 
-<nav>
-    <ul class="nav style">
-        @foreach($navbarItems as $item)
-            <li class="nav-item">
-                <a
-                    class="nav-link text-dark {{ $item['active'] ? 'active' : '' }}"
-                    href="{{ $item['href'] }}"
-                >
-                    {{ $item['label'] }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
-</nav>
+<header class="container">
+    <div class="d-flex align-items-center py-2 justify-content-between">
+
+        <img
+            src="{{ Vite::asset('resources/img/dc-logo.png') }}"
+            alt="DC Comics Logo"
+            class="logo-dc"
+        >
+
+        <nav>
+            <ul class="nav style">
+                @foreach($navbarItems as $item)
+                    <li class="nav-item">
+                        <a
+                            class="nav-link text-dark {{ $item['active'] ? 'active' : '' }}"
+                            href="{{ $item['href'] }}"
+                        >
+                            {{ $item['label'] }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </nav>
+
+    </div>
+</header>
